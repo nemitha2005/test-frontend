c7dfe68c94376533ff7527d45d33aa9b01a18d57
nemitha2005 <nemithan05@gmail.com>
2024-12-10 20:13:38 +0530

    update css

--- system-frontend/src/App.js
+++ system-frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import ConfigurationForm from "./components/ConfigurationForm";
 import ControlPanel from "./components/ControlPanel";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import LogDisplay from "./components/LogDisplay";
 
@@ -12,6 +13,7 @@ function App() {
       <ConfigurationForm />
       <ControlPanel />
         <LogDisplay />
+        <Footer />
     </div>
   );
 }
---
+++ system-frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <div className="footer-container">
+            <p>© 2024 L5 OOP ICW Ticket Simulation Control Panel</p>
+            <p>Created by S.M.N.N. Wijerathna - 20222001</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
